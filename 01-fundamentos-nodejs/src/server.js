7890e308d7fba5a8247dc3e830c0938d94dc18d7
felipeffranco <felipeffranco@outlook.com>
2023-05-03 23:08:01 -0300

    server --watch mode

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -7,7 +7,7 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => { 
-    return response.end('Hello World')
+    return response.end('Hello Ignite')
 })
 
 server.listen(3333)
